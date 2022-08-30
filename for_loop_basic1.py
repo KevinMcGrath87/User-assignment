109380afa8d8f8b244ad5e813737e09dea465f13
Kevin McGrath <kpmcgra@gmail.com>
2022-08-30 15:37:40 -0500

    loop basics

---
+++ for_loop_basic1.py
@@ -0,0 +1,38 @@
+# #for loop basics
+# for x in range(0, 151):
+#     print(x)
+
+# x = 5
+# while x <= 1000:
+#     print(x)
+#     x=x*5
+
+# for x in range( 0, 1005, 5):
+#     print(x)
+
+# for x in range(1, 101):
+#     if x%10 == 0:
+#         print("Coding Dojo")
+#     elif x%5 == 0:
+#         print("Coding")
+#     else:
+#         print(x)
+
+# run = 0
+# for x in range(1, 500000, 2):
+#     if x < 100:
+#      print(x)
+#     run = x+run
+# print(run)
+
+
+
+# for x in range(2018, 0, -4):
+#     print(x)
+
+lowNum = 180
+highNum = 300
+mult = 7
+for x in range(lowNum, highNum):
+    if x%7 == 0 :
+        print(x)
